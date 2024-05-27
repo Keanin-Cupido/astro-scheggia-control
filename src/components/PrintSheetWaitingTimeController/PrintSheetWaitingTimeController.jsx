7512474dcfb053c6bdf52d06b0c115400af4dea7
Keanin Cupido <keanin16@gmail.com>
2024-05-27 22:34:17 +0200

    ...

---
+++ src/components/PrintSheetWaitingTimeController/PrintSheetWaitingTimeController.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PrintSheetWaitingTimeController = () => {
+  return (
+    <div>PrintSheetWaitingTimeController</div>
+  )
+}
+
+export default PrintSheetWaitingTimeController;
---
+++ src/components/RollerSpeedController/RollerSpeedController.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RollerSpeedController = () => {
+  return (
+    <div>RollerSpeedController</div>
+  )
+}
+
+export default RollerSpeedController;
